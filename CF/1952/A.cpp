b3936d3995c409121992457e0e6ef726a7f70134
Niraj Modi <modiniraj1034@gmail.com>
2024-04-06 22:50:21 +0530

    1950 & 1952

--- CF/1952/A.cpp
+++ CF/1952/A.cpp
@@ -6,10 +6,16 @@ const int mod = 1e9+7;
 int main()
 {
     int t;
+    cin>>t;
     while(t--){
-        int n;
-        cin>>n;
-        for()
+        string s;
+        cin>>s;
+        int k=0;
+        for(char ch:s){
+            k += (ch-'a');
+        }
+        cout<<k<<" ";
+        cout<<"YES\n";
     }
     return 0;
 }
